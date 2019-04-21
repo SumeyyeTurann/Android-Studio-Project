3a94472d9437b96761a4eeb57f718d31a4f635cb
Sümeyye Turan <36699340+SumeyyeTurann@users.noreply.github.com>
2019-04-21 18:34:29 +0300

    Gezgin Dostum Uygulama
    firebase bağlantısı kurarak gezgin dosyum uygulaması geliştirildi

--- SanatDefterimUygulamasi/app/src/main/java/application/sumeyyeturan/com/sanatdefterimuygulamasi/MainActivity.java
+++ SanatDefterimUygulamasi/app/src/main/java/application/sumeyyeturan/com/sanatdefterimuygulamasi/MainActivity.java
@@ -1,5 +1,6 @@
 package application.sumeyyeturan.com.sanatdefterimuygulamasi;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SQLiteDatabase myDataBase = this.openOrCreateDatabase("Musician",MODE_PRIVATE,null);
+        try{
+
+            SQLiteDatabase myDataBase = this.openOrCreateDatabase("Musician",MODE_PRIVATE,null);
+            myDataBase.execSQL("CREATE TABLE IF NOT EXISTS musicians (name VARCHAR, age INT(2))");
+            //myDataBase.execSQL("INSERT INTO musicians (name, age) VALUES ('James', 50)");
+
+            Cursor cursor = myDataBase.rawQuery("SELECT * FROM musicians",null);
+
+            int nameIx = cursor.getColumnIndex("name");
+            int ageIx = cursor.getColumnIndex("age");
+            cursor.moveToFirst(); //cursoru sayfanın en başına götürdü
+
+            while(cursor != null){ //cursor boş bir satıra gelmediği sürece devam et
+                System.out.println("Name: " + cursor.getString(nameIx));
+                System.out.println("Age: " + cursor.getInt(ageIx));
+                
+                cursor.moveToNext();
+            }
+
+        }catch (Exception e){
+            e.printStackTrace(); //bir hata ilr karşılaşınca hatayı monitörde yaz
+        }
+
+
     }
 }
